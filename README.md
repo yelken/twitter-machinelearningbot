c8e8334d813cc2cfe4b530385555c18fcd1c518c
Yelken Gonzales <yelkezin@gmail.com>
2016-09-03 18:29:29 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+Doutor Política
+=========
+
+Sobre
+-----
+O Doutor Politica é um robô que conversa com eleitores e políticos onde é possível promover debates, conversar sobre política e tirar dúvidas. Tudo isso é feito com Machine Learning da IBM e integrado um BOT pelo Twitter que depois pode ser integrado ao Facebook.
---
+++ service.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import time
+
+from markovbot import MarkovBot
+
+tweetbot = MarkovBot()
+
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+book = os.path.join(dirname, u'Freud_Dream_Psychology.txt')
+tweetbot.read(book)
+
+
+my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
+
+print(u'\ntweetbot says: "%s"' % (my_first_text))
+
+
+# Consumer Key (API Key)
+cons_key = '1rVz2u5o37F6ycuQiXqYpmvz7'
+# Consumer Secret (API Secret)
+cons_secret = 'r1z2vJLwUExkWDm0vgVKgUYLT5cB5jRcUQDxizbBp6jIYGv4Op'
+# Access Token
+access_token = '772074137258954752-g9yoZSIRVmhA9EQmyAfDAx5Hfef1G7w'
+# Access Token Secret
+access_token_secret = 'sqkQRyOWKSXXSxZHqVWwKZZoJpXHnLar7nlT2z1O1ot8Z'
+
+# Log in to Twitter
+tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
+
+targetstring = 'professorpolitica'
+
+keywords = ['política', 'educação', 'saúde', 'pobreza', 'eleições']
+
+prefix = None
+suffix = None
+maxconvdepth = None
+
+tweetbot.twitter_autoreply_start(targetstring, keywords=keywords, prefix=prefix, suffix=suffix, maxconvdepth=maxconvdepth)
+ 
+
+tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=2, keywords=None, prefix=None, suffix='#hackathonSprinklr')
+
+secsinweek = 7 * 24 * 60 * 60
+time.sleep(secsinweek)
+ 
+tweetbot.twitter_autoreply_stop()
+
+tweetbot.twitter_tweeting_stop()
